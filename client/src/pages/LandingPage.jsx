8f942ebc1868abdca229db69811add5cf8cbf3ce
RMvistar <r.marcovistar@gmail.com>
2025-08-28 14:17:53 +0800

    Updated LandingPage

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import "./styles/LandingPageStyle.css";
-import landingImage from "./images/feature-pic-01-5.png"; // If inside src folder
+import landingImage from "./images/feature-pic-01-5.png";
+import BackgroundImage from "./images/background_img.png";
 
 function LandingPage() {
   return (
-    <div className="landing-container">
+    <div
+      className="landing-container"
+      style={{
+        backgroundImage: `url(${BackgroundImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
       <header className="navbar">
         <div className="navbar-logo">RDJ</div>
         <nav className="navbar-links">
@@ -28,8 +37,45 @@ function LandingPage() {
           <img
             src={landingImage}
             alt="Clog Detection System"
-            //style={{ width: "300px", marginBottom: "2rem" }}
+            style={{
+              boxShadow: "0 8px 32px 0 rgba(8, 16, 131, 0.25)",
+              borderRadius: "16px",
+              width: "800px",
+              maxWidth: "100%",
+              height: "auto",
+            }}
           />
+        </section>
+        <section className="landing-background">
+          <h2>Background of the Study</h2>
+          <p>
+            The Philippines is a tropical country that frequently experiences
+            heavy rainfall and an average of 20 tropical cyclones (PAGASA,
+            n.d.). This indicates that the Philippines is prone to experience
+            flooding, especially in continuous occurrence of inclement weather
+            conditions. Developing regions in the Philippines, including Region
+            VI focus their efforts on urbanization and infrastructure
+            development. One of the challenges faced by urbanization in tropical
+            countries that experience heavy rainfalls includes uncontrolled
+            flooding resulting in flood loss, especially when weather parameters
+            are vague. The Philippines records approximately 200,000 deaths
+            caused by weather-related disasters between 2000 and 2019 (Gray et
+            al., 2022). The NOAA National Severe Storms Laboratory of the US
+            Government (n.d.) stated that “floods are the most common and
+            widespread of all weather-related natural disasters.” Therefore,
+            floods must be monitored and predicted in order to mitigate the
+            damage brought by typhoons and heavy rainfall. According to an
+            article in ReliefWeb, written by the AHA Centre (2023), on February
+            15, 2023, Region VI suffered flooding in 51 barangays, wherein 1,221
+            families were affected. In the same article written by the AHA
+            Centre (2023), 13 roads and 1 bridge were affected. This implies
+            that Filipinos, especially commuters, suffered inconvenience and
+            exposed themselves to hazardous risk caused by flooding.
+            Insufficient information and warning of recurring events such as
+            flooding, pose a major challenge on the safety of Filipino
+            communities. Hence, the proponents aim to develop a machine
+            learning-powered clog and flow rate flood detection system.
+          </p>
         </section>
       </main>
     </div>
